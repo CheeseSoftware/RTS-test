9f681aa8e6a41a63ff1c3e45747ae5acd86ebba3
ostkaka <john.ostkaka@gmail.com>
2015-08-13 16:25:49 +0200

    InputManager. (untested)

---
+++ RTS-test/Types/Event.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RTS_test.Types
+{
+    public class VoidEvent
+    {
+        public delegate void VoidDelegate();
+        event VoidDelegate theEvent;
+
+        public void call()
+        {
+            theEvent();
+        }
+
+        public void subscribeEvent(VoidDelegate voidDelegate)
+        {
+            theEvent += voidDelegate;
+        }
+
+        public void unsubscribeEvent(VoidDelegate voidDelegate)
+        {
+            theEvent -= voidDelegate;
+        }
+    }
+}
